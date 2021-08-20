d6bedafd537675acbfddc9d773db84480bbbc1c9
André Luis <andre.luis.e3@gmail.com>
2021-08-20 12:06:15 -0300

    Deploy Initialized

---
+++ API/VehicleInsurance/VehicleInsurance.py
@@ -0,0 +1,54 @@
+import numpy as np 
+import pandas as pd 
+import pickle
+
+class VehicleInsurance:
+
+    def __init__(self):
+        self.home_path = r'C:\Users\andre_\OneDrive\GitHub\insurance-cross-sell'
+        self.annual_premium_scaler =            pickle.load( open(self.home_path + r'\Parameters\annual_premium_scaler.pkl','rb'))
+        self.age_scaler =                       pickle.load( open(self.home_path + r'\Parameters\age_scaler.pkl','rb'))
+        self.vintage_scaler =                   pickle.load( open(self.home_path + r'\Parameters\vintage_scaler.pkl','rb'))
+        self.region_code_encoder =              pickle.load( open(self.home_path + r'\Parameters\region_code_encoder.pkl','rb'))
+        self.policy_sales_channel_encoder =     pickle.load( open(self.home_path + r'\Parameters\policy_sales_channel_encoder.pkl','rb'))
+
+    def data_cleaning(self,data):
+
+        lower = lambda x: x.lower()
+        data.columns = list(map(lower,data.columns))
+        data['region_code'] = data['region_code'].astype('int64')
+        data['policy_sales_channel'] = data['policy_sales_channel'].astype('int64')
+
+        data = data.drop('id',axis=1)
+        data.index = data.index + 1
+
+        return data
+
+    def feature_engineering(self,data):
+        rule = lambda x: 1 if (x['vehicle_damage'] == 'Yes') & (x['previously_insured'] == 0) else 0 
+        data['no_insured_damage'] = data.apply(rule,axis=1)
+
+        return data
+
+    def data_preparation(self,data):
+
+        vehicle_damage_dict = {'No' : 0, 'Yes' : 1}
+        data['vehicle_damage'] = data['vehicle_damage'].map(vehicle_damage_dict)
+
+        data['annual_premium'] = self.annual_premium_scaler.transform(data['annual_premium'].values)
+        data['age'] = self.age_scaler.transform(data['age'].values)
+        data['vintage'] = self.vintage_scaler.transform(data['vintage'].values)
+        
+        data['region_code'] = data['region_code'].map(self.region_code_encoder)
+        data['policy_sales_channel'] = data['policy_sales_channel'].map(self.policy_sales_channel_encoder)
+
+        selected_columns = ['vintage','annual_premium','age','region_code','policy_sales_channel','no_insured_damage','vehicle_damage','previously_insured']
+
+        return data[selected_columns]
+
+    def get_prediction(self,model,original_data,test_data):
+
+        pred = model.predict_proba(test_data)
+        original_data['score'] = pred[:,1].tolist()
+
+        return original_data.to_json(orient='records', date_format='iso')
---
+++ API/handler.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+import pickle
+from flask import Flask,request, Response
+from VehicleInsurance.VehicleInsurance import VehicleInsurance
+import os
+
+
+path = r'C:\Users\andre_\OneDrive\GitHub\insurance-cross-sell'
+model = pickle.load(open(path + r'\Model\xgb_model.pkl','rb'))
+
+app = Flask(__name__)
+
+@app.route('/predict',methods=['POST'])
+
+def health_insurance_predict():
+    test_json = request.get_json()
+
+    if test_json:
+        if isinstance(test_json,dict):
+            test_raw =pd.DataFrame(test_json, index=[0])
+        
+        else:
+            test_raw = pd.DataFrame(test_json, columns=test_json[0].keys())
+
+        pipeline = VehicleInsurance()
+
+        data_cleaned = pipeline.data_cleaning(test_raw)
+        data_engineered = pipeline.feature_engineering(data_cleaned)
+        data_prepared = pipeline.data_preparation(data_engineered)
+
+        df_response = pipeline.get_prediction(model, test_raw, data_prepared)
+
+        return df_response
+
+    else:
+        return Response('{}',status=200,mimetype='application/json')
+
+if __name__ == '__main__':
+    port = os.environ.get('PORT',5000)
+    app.run(host = '127.0.0.1',port=port)
